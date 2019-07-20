0cc4768dc4848368a15c9920616e1db1ae3d7958
andy_hardy <aohardy@gmail.com>
2019-07-20 11:15:45 -0600

    working on api routes to get backend functional before adding react

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,28 +14,35 @@ router.get("*", (req, res) => {
 // set up some api routes for the front end
 
 router.get("/api/scrape", (req, res) => {
-    console.log("router works");
     axios.get("https://fasterskier.com/").then(response => {
         const $ = cheerio.load(response.data);
-
         const results = [];
         $("article").each((i, article) => {
-            let fullPara = $(article).find(".entry-content>p").text();
-            let description = fullPara;
-            if (fullPara.length > 200) {
-                let endIndex = fullPara.indexOf(" ", 200);
-                description = fullPara.slice(0, endIndex) + "..."; 
-            }
-            const data = {};
-            data.header = $(article).find(".entry-title").text();
-            data.href = $(article).find(".entry-title-link").attr("href");
-            data.img = $(article).find("img").attr("src");
-            data.description = description;
-            data.published = $(article).find("time").attr("datetime");
+            let data = scraperHelper(article);
             results.push(data);
         });
         res.json(results);
     });
 });
+
+router.get("/api/saved", (req, res) => {
+    // get saved articles from the database and show them on the page.
+})
+
+const scraperHelper = (article) => {
+    let fullPara = $(article).find(".entry-content>p").text();
+    let description = fullPara;
+    if (fullPara.length > 200) {
+        let endIndex = fullPara.indexOf(" ", 200);
+        description = fullPara.slice(0, endIndex) + "...";
+    }
+    let data = {};
+    data.header = $(article).find(".entry-title").text();
+    data.href = $(article).find(".entry-title-link").attr("href");
+    data.img = $(article).find("img").attr("src");
+    data.description = description;
+    data.published = $(article).find("time").attr("datetime");
+    return data;
+}
 
 module.exports = router;
